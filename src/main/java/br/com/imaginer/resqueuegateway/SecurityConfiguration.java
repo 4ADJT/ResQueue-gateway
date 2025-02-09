9d73af6f2f131d6b62cafe16bda3b3bef8223a85
rodrigobrocchi <rodrigo.brocchi@gmail.com>
2025-02-08 21:46:01 -0300

    ✨ feat(deploy.yml): Add OAuth2 JWT configuration for gateway
    ✨ feat(pom.xml): Add Spring Security OAuth2 Jose dependency
    
    ✨ feat(application.yml): Update OAuth2 issuer URI and Eureka URL environment variables
    
    ✨ feat(SecurityConfiguration.java): Enable WebFlux security with JWT configuration

--- src/main/java/br/com/imaginer/resqueuegateway/SecurityConfiguration.java
+++ src/main/java/br/com/imaginer/resqueuegateway/SecurityConfiguration.java
@@ -2,12 +2,11 @@ package br.com.imaginer.resqueuegateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -21,14 +20,17 @@ public class SecurityConfiguration {
         .csrf(ServerHttpSecurity.CsrfSpec::disable)
 
         .authorizeExchange(exchanges -> exchanges
-            .pathMatchers("/eureka*/**").permitAll()
-            .pathMatchers("/actuator*/**").permitAll()
-            .pathMatchers("/login").permitAll()
-            .pathMatchers("/create/user/**").permitAll()
-            .pathMatchers("/logout*/**").permitAll()
+            .pathMatchers("/**/eureka/**").permitAll()
+            .pathMatchers("/**/actuator/**").permitAll()
+            .pathMatchers("/**/login").permitAll()
+            .pathMatchers("/**/create/admin").permitAll()
+            .pathMatchers("/**/create/group", "/**/create/user", "/**/list/user")
+              .hasAuthority("ADMIN")
+            .pathMatchers("/**/logout").permitAll()
             .anyExchange().authenticated()
+
         ).oauth2ResourceServer(oauth2 -> oauth2
-            .jwt(withDefaults())
+            .jwt(Customizer.withDefaults())
         );
 
     return http.build();
