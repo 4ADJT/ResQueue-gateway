620fb93f227961688dc6b7168ea90c5ebe886f31
rodrigobrocchi <rodrigo.brocchi@gmail.com>
2025-02-09 21:24:06 -0300

    [✨ feat](file/topic): Rename SecurityConfiguration class to config/SecurityConfiguration and add JwtProperties configuration class.

--- src/main/java/br/com/imaginer/resqueuegateway/config/SecurityConfiguration.java
+++ src/main/java/br/com/imaginer/resqueuegateway/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package br.com.imaginer.resqueuegateway;
+package br.com.imaginer.resqueuegateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,14 +6,20 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfiguration {
+
+  private final JwtProperties jwtProperties;
+
+  public SecurityConfiguration(JwtProperties jwtProperties) {
+    this.jwtProperties = jwtProperties;
+  }
 
   @Bean
   public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
@@ -36,5 +42,10 @@ public class SecurityConfiguration {
         );
 
     return http.build();
+  }
+
+  @Bean
+  public ReactiveJwtDecoder jwtDecoder() {
+    return ReactiveJwtDecoders.fromIssuerLocation(jwtProperties.getIssuerUri());
   }
 }
